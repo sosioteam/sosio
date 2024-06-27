2f0688761eebaa4cbb5671492d73eae3efccb8b9
Tuna Han Berber <83482995+TunahanBerber@users.noreply.github.com>
2024-06-27 12:25:12 +0300

    added image packs & Banner components (#3)
    * added image packs & Banner components
    
    * Changing image names and making the requested changes in the banner
    
    * add banner component and install tailwind
    
    * clear don't use import
    
    * Remove test class
    
    * Remove test class and edit banner tag
    
    * banner-remove don't use style
    
    ---------
    
    Co-authored-by: tunahan.berber

---
+++ app/components/banner.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div className="flex items-center bg-white text-black w-full h-[80vh]"></div>
+  );
+};
+
+export default Banner;
