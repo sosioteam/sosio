bdc12015c94d5dc2f36d3228f0ad24bd8b1f1b4f
tunahan.berber <tunahan.berber@eba.gov.tr>
2024-06-27 10:48:32 +0300

    Remove test class

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 import Banner from "./components/banner";
 
 export default function Home() {
-  return (
-    <div className="test">
-      <Banner></Banner>
-    </div>
-  );
+  return <Banner></Banner>;
 }
