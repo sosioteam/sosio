00c0fb9bac27ba4748d785f45b7a70df684d3b0c
tunahan.berber <tunahan.berber@eba.gov.tr>
2024-06-29 22:21:50 +0300

    Banner left-and right content

--- app/components/banner.tsx
+++ app/components/banner.tsx
@@ -2,7 +2,15 @@ import React from "react";
 
 const Banner = () => {
   return (
-    <div className="flex items-center bg-white text-black w-full h-[80vh]"></div>
+    <div className="flex items-center bg-white text-black w-full h-[80vh]">
+      {/* Left Area (2/3 of the width) */}
+      <div className="w-2/3 h-full bg-white">{/* !Left Content go here */}</div>
+
+      {/* Right Area (1/3 of the width) */}
+      <div className="w-1/3 h-full bg-teal-400">
+        {/* !Right Content go here */}
+      </div>
+    </div>
   );
 };
 
