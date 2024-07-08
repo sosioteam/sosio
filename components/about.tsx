b3f49a6f92e91f6a9d3a029498a2674f373bb49c
tunahan.berber <tunahan.berber@eba.gov.tr>
2024-07-08 20:36:04 +0300

    about-mobile-responsive

--- components/about.tsx
+++ components/about.tsx
@@ -3,13 +3,12 @@ import Image from "next/image";
 
 const About = () => {
   return (
-    <div className="flex flex-col items-center bg-white text-black w-full h-[70vh] overflow-hidden">
-      {/* Title and description container */}
-      <div className="w-9/12 bg-white h-1/2 flex flex-col items-center justify-center">
-        <h1 className="text-2xl font-bold mb-4">
+    <div className="flex flex-col items-center bg-white text-black w-full h-screen overflow-hidden">
+      <div className="w-full md:w-9/12 bg-white h-1/2 flex flex-col items-center justify-center p-4">
+        <h1 className="text-xl md:text-2xl font-bold mb-4 text-center">
           Did you know that 2 out of 5 people lie about liking a gift?
         </h1>
-        <p className="text-center mx-auto px-6">
+        <p className="text-sm md:text-base text-center mx-auto px-2 md:px-6">
           Every year 20 billion worth of unwanted gifts clutter our houses...
           and our planet. Over 30% of gift cards that we give to each other -
           never get redeemed. As they expire or get lost in our kitchen drawers,
@@ -18,23 +17,25 @@ const About = () => {
           It&#39;s time for a new gifting solution that fulfills dreams.
         </p>
       </div>
-      {/* Image container */}
       <div className="flex w-full justify-center gap-4 p-4">
         <Image
           src="/sosioimg/give.svg"
           alt="logo-img"
+          className="w-1/3 h-auto md:w-auto"
           width={200}
           height={100}
         />
         <Image
           src="/sosioimg/receive.svg"
           alt="logo-img"
+          className="w-1/3 h-auto md:w-auto"
           width={200}
           height={100}
         />
         <Image
           src="/sosioimg/achieve.svg"
           alt="logo-img"
+          className="w-1/3 h-auto md:w-auto"
           width={200}
           height={100}
         />
