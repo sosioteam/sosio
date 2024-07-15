36d95f79de8cbc4f6230f565970f3e4cfe375892
tunahan.berber <tunahan.berber@eba.gov.tr>
2024-07-15 17:03:55 +0300

    color-full-div

---
+++ components/center-banner.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Image from "next/image";
+
+const CenterBanner = () => {
+  return (
+    <div className="flex items-center justify-center bg-white text-white w-full h-[30vh] overflow-hidden ">
+      <div className="flex items-center justify-around w-[70vw] h-[20vh] bg-colorFullDiv bg-cover rounded-[5rem] ">
+        <p className="text-3xl font-bold">Try Boon & Bliss, it’s free!</p>
+        <Image
+          src="/sosioimg/app-store.svg"
+          alt="app-store-img"
+          width={200}
+          height={100}
+        />
+        <Image
+          src="/sosioimg/play-store.svg"
+          alt="play-store-img"
+          width={200}
+          height={100}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default CenterBanner;
