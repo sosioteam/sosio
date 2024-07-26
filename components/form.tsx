1ba05e34f815ecae558005c1fc6b4a8e657173bf
Tuna Han Berber <83482995+TunahanBerber@users.noreply.github.com>
2024-07-26 10:14:38 +0300

    Question component (#9)
    * all-questions-component-design
    
    * merge-update-question and answer
    
    * form
    
    ---------
    
    Co-authored-by: tunahan.berber <tunahan.berber@eba.gov.tr>
    Co-authored-by: Deniz Muratoğlu <denizmrtoglu@hotmail.com>

---
+++ components/form.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+const Form = () => {
+  return (
+    <div className="w-full  mx-auto mt-10">
+      <div className="bg-cardHeader p-4 rounded-t-2xl">
+        <h2 className="text-2xl font-bold text-center">Ask us anything</h2>
+      </div>
+      <form className="bg-white p-6 rounded-b-lg shadow-md space-y-4">
+        <div className="flex flex-row justify-between space-x-4">
+          <div className="flex flex-col space-y-2 w-1/2">
+            <label className="font-semibold text-black">Name</label>
+            <input
+              type="text"
+              placeholder="Name"
+              className="p-3 border border-input rounded-2xl focus:outline-none focus:ring-2 focus:inputFocus w-full"
+            />
+          </div>
+          <div className="flex flex-col space-y-2 w-1/2">
+            <label className="font-semibold text-black">Email</label>
+            <input
+              type="email"
+              placeholder="Email"
+              className="p-3 border border-input rounded-2xl focus:outline-none focus:ring-2 focus:inputFocus w-full"
+            />
+          </div>
+        </div>
+
+        <div className="flex flex-col space-y-2">
+          <label className="font-semibold text-black">Your question</label>
+          <textarea
+            placeholder="Your app is perfect!!"
+            className="p-2 border border-input rounded-lg h-24 resize-none focus:outline-none focus:ring-2 focus:inputFocus"
+          ></textarea>
+        </div>
+        <div className="flex flex-col space-y-2 items-center justify-center">
+          <button
+            type="submit"
+            className="w-40 p-3 bg-black text-white rounded-2xl hover:bg-gray-700 transition duration-300"
+          >
+            Send
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
--- components/question/question-center.tsx
+++ components/question/question-center.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import Image from "next/image";
 import QuestionAnswer from "./question-answer";
 import QuestionHeader from "./question-header";
+import Form from "../form";
 
 const QuestionCenter = () => {
   return (
     <div className="flex h-full flex-col w-full md:w-2/3  mt-10 justify-normal items-start md:items-start p-4 md:p-0">
       <QuestionHeader />
       <QuestionAnswer />
+      <Form />
     </div>
   );
 };
